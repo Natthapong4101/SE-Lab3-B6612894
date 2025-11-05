895d35ad63fea14bff896b9d08e8a51047fa3eef
Natthapong4101 <mostnpt14@gmail.com>
2025-11-05 11:48:14 +0700

    Entity Playlist - close #19

--- models/student.go
+++ models/student.go
@@ -5,7 +5,5 @@ import "gorm.io/gorm"
 type Student struct {
 	gorm.Model
 	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Age       int    `json:"age"`
-	Email     string `json:"email"`
+	LastName  string `json:"last_name"` 
 }
